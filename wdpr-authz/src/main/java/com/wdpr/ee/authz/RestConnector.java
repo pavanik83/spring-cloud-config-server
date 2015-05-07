520a6889e94457ccc608db59cddfb44dfb899260
archj009 <jack.archer@disney.com>
2015-05-07 15:31:10 -0400

    Added PoolCacheManger for authz call HTTP client

--- wdpr-authz/src/main/java/com/wdpr/ee/authz/RestConnector.java
+++ wdpr-authz/src/main/java/com/wdpr/ee/authz/RestConnector.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.cache.CacheResponseStatus;
 import org.apache.http.client.cache.HttpCacheContext;
@@ -16,9 +17,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.HttpClientUtils;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.cache.CacheConfig;
 import org.apache.http.impl.client.cache.CachingHttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -74,53 +78,94 @@ public class RestConnector {
 	int TIME_OUT;
 
 	/**
-     * Maps from JSON response to Java object AuthDO
-     */
+	 * Maps from JSON response to Java object AuthDO
+	 */
 	ObjectMapper mapper = new ObjectMapper();
 	/**
-     * Configures timeouts and stale connection check
-     */
+	 * Configures timeouts and stale connection check
+	 */
 	static RequestConfig defaultRequestConfig;
-    /**
-    * Configures max cache entries, size
-    */
-    static CacheConfig cacheConfig;
-    /**
-    * Adds cache config and request config to Apache http client
-    */
-    static CloseableHttpClient httpClient;
+	/**
+	 * Configures max cache entries, size
+	 */
+	static CacheConfig cacheConfig;
+	static PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+	/**
+	 * Adds cache config and request config to Apache http client
+	 */
+	// static CloseableHttpClient httpClient;
+	CloseableHttpClient httpClient = HttpClients.custom()
+			.setConnectionManager(connManager).build();
 
 	@SuppressWarnings("deprecation")
 	private RestConnector() {
-		defaultRequestConfig = RequestConfig.custom()
-				.setSocketTimeout(this.TIME_OUT)
-				.setConnectTimeout(this.TIME_OUT)
-				.setConnectionRequestTimeout(this.TIME_OUT)
-				.setStaleConnectionCheckEnabled(true).build();
+		// Set some shared constants
+		initSharedConstatnts();
+		// Set the HttpClient cache
+		initCache();
+		httpClient = CachingHttpClients.custom().setCacheConfig(cacheConfig)
+				.setDefaultRequestConfig(defaultRequestConfig).build();
+		// Set the HttpCleint connection pool
+		initPoolManager();
+
+		// Set teh AuthZ and configuration (scope.json) parameters for this
+		// filter
+		this.AUTH_PATH = config.getPropertyVal(AuthConstants.AUTH_CTX_PATH);
+		this.SCOPE_PATH = config.getPropertyVal(AuthConstants.SCOPE_CTX_PATH);
+		StringBuilder settings = new StringBuilder(
+				"####The setting for the RestConnection in the AuthZ Filter are:");
+		settings.append("AUTH_PATH = ");
+		settings.append(this.AUTH_PATH + "; ");
+		settings.append("SCOPE_PATH = ");
+		settings.append(this.SCOPE_PATH + "; ");
+		settings.append("HOST = ");
+		settings.append(this.HOST + "; ");
+		// LOG.debug(settings.toString());
+	}
+
+	/**
+	 * Set the constants used to configure the HttpClient connections, cache,
+	 * and scope validation
+	 */
+	private void initSharedConstatnts() {
+
 		this.PROTOCOL = config.getPropertyVal(AuthConstants.PROTOCOL);
 		this.PORT = Integer.parseInt(config.getPropertyVal(AuthConstants.PORT)
 				.trim());
 		this.TIME_OUT = Integer.parseInt(config.getPropertyVal(
 				AuthConstants.TIME_OUT).trim());
 		this.HOST = config.getPropertyVal(AuthConstants.HOST);
+		// Set teh AuthZ and configuration (scope.json) parameters for this
+		// filter
 		this.AUTH_PATH = config.getPropertyVal(AuthConstants.AUTH_CTX_PATH);
 		this.SCOPE_PATH = config.getPropertyVal(AuthConstants.SCOPE_CTX_PATH);
-        cacheConfig = CacheConfig.custom()
-                .setMaxCacheEntries(1000)
-                .setMaxObjectSize(8192)
-                .build();
-        httpClient = CachingHttpClients.custom()
-                .setCacheConfig(cacheConfig)
-                .setDefaultRequestConfig(defaultRequestConfig)
-                .build();
-        StringBuilder settings = new StringBuilder("####The setting for the RestConnection in the AuthZ Filter are:");
-        settings.append("AUTH_PATH = ");
-        settings.append(this.AUTH_PATH + "; ");
-        settings.append("SCOPE_PATH = ");
-        settings.append(this.SCOPE_PATH + "; ");
-        settings.append("HOST = ");
-        settings.append(this.HOST + "; ");
-//        LOG.debug(settings.toString());
+
+	}
+
+	/**
+	 * Set the connection pool parameters for the HttpClient used to access the
+	 * AuthZ server
+	 */
+	private void initPoolManager() {
+		connManager.setMaxTotal(10);
+		connManager.setDefaultMaxPerRoute(10);
+		HttpHost host = new HttpHost(this.HOST, this.PORT);
+		connManager.setMaxPerRoute(new HttpRoute(host), 10);
+	}
+
+	/**
+	 * Set teh Http Client caching params for the HttpClient used to access the
+	 * AuthZ server
+	 */
+	private void initCache() {
+		defaultRequestConfig = RequestConfig.custom()
+				.setSocketTimeout(this.TIME_OUT)
+				.setConnectTimeout(this.TIME_OUT)
+				.setConnectionRequestTimeout(this.TIME_OUT)
+				.setStaleConnectionCheckEnabled(true).build();
+
+		cacheConfig = CacheConfig.custom().setMaxCacheEntries(1000)
+				.setMaxObjectSize(8192).build();
 	}
 
 	/**
@@ -141,7 +186,8 @@ public class RestConnector {
 	public String callGoDotComValidateToken(Map<String, String> tokenList)
 			throws IOException {
 		// We need the response to validate scope authorizations
-//		LOG.debug("####Token list in callGoDotComValidateToken() = " + tokenList.toString());
+		// LOG.debug("####Token list in callGoDotComValidateToken() = " +
+		// tokenList.toString());
 		return callGoDotComGet(tokenList, this.AUTH_PATH);
 	}
 
@@ -152,7 +198,8 @@ public class RestConnector {
 	 */
 	public TokenDO callGoDotComValidateScope(Map<String, String> tokenList)
 			throws IOException {
-//		LOG.debug("####Token list in callGoDotComValidateScope() = " + tokenList.toString());
+		// LOG.debug("####Token list in callGoDotComValidateScope() = " +
+		// tokenList.toString());
 
 		TokenDO tokenObj = callGoDotComPost(tokenList, this.SCOPE_PATH,
 				TokenDO.class);
@@ -169,11 +216,11 @@ public class RestConnector {
 			throws IOException {
 		String json = null;
 		String accessToken = null;
-        CloseableHttpResponse response = null;
+		CloseableHttpResponse response = null;
 		try {
 			URIBuilder builder = new URIBuilder();
 			if (ctxPath.equals(this.AUTH_PATH)) {
-//				LOG.debug(tokenList);
+				// LOG.debug(tokenList);
 				// Some confusion between header value access_token and
 				// authorization
 				accessToken = tokenList.get(AuthConstants.ACCESS_TOKEN);
@@ -200,16 +247,16 @@ public class RestConnector {
 					.setPort(this.PORT).setPath(ctxPath);
 
 			HttpCacheContext context = HttpCacheContext.create();
-	        HttpGet getRequest = new HttpGet(builder.build());
-			//HttpResponse response = httpClient.execute(getRequest);
-	        double start = System.nanoTime();
-	        response = httpClient.execute(getRequest, context);
+			HttpGet getRequest = new HttpGet(builder.build());
+			// HttpResponse response = httpClient.execute(getRequest);
+			double start = System.nanoTime();
+			response = httpClient.execute(getRequest, context);
 			double end = System.nanoTime();
 			double elapsed = end - start;
 
-			LOG.debug( "#### Time for calling the AUTHZ server is: "
+			LOG.debug("#### Time for calling the AUTHZ server is: "
 					+ Double.toString(elapsed / 1000000) + " milliseconds");
-			
+
 			int statusCode = response.getStatusLine().getStatusCode();
 			StringBuilder authZcallMsg = new StringBuilder();
 
@@ -221,35 +268,39 @@ public class RestConnector {
 				authZcallMsg.append("Validation FAILED for token ");
 				authZcallMsg.append(accessToken);
 			}
-//            CacheResponseStatus responseStatus = context.getCacheResponseStatus();
-//            switch (responseStatus) {
-//                case CACHE_HIT:
-//                    LOG.debug("Cache Hit: A response was generated from the cache with " +
-//                            "no requests sent upstream");
-//                    break;
-//                case CACHE_MODULE_RESPONSE:
-//                    LOG.debug("Cache Response: The response was generated directly by the " +
-//                            "caching module");
-//                    break;
-//                case CACHE_MISS:
-//                    LOG.debug("Cache Miss: The response came from an upstream server");
-//                    break;
-//                case VALIDATED:
-//                    LOG.debug("Cache Validated: The response was generated from the cache " +
-//                            "after validating the entry with the origin server");
-//                    break;
-//                default:
-//                    LOG.warn("No response cache status: " +
-//                            responseStatus);
-//                    break;
-//            }
+			 CacheResponseStatus responseStatus =
+			 context.getCacheResponseStatus();
+			 switch (responseStatus) {
+			 case CACHE_HIT:
+			 LOG.debug("Cache Hit: A response was generated from the cache with "
+			 +
+			 "no requests sent upstream");
+			 break;
+			 case CACHE_MODULE_RESPONSE:
+			 LOG.debug("Cache Response: The response was generated directly by the "
+			 +
+			 "caching module");
+			 break;
+			 case CACHE_MISS:
+			 LOG.debug("Cache Miss: The response came from an upstream server");
+			 break;
+			 case VALIDATED:
+			 LOG.debug("Cache Validated: The response was generated from the cache "
+			 +
+			 "after validating the entry with the origin server");
+			 break;
+			 default:
+			 LOG.warn("No response cache status: " +
+			 responseStatus);
+			 break;
+			 }
 			authZcallMsg.append(" Response SC:" + statusCode + ", from (GET)"
 					+ getRequest.getURI().toString() + " response=" + json);
-//			LOG.debug(authZcallMsg.toString());
+			// LOG.debug(authZcallMsg.toString());
 		} catch (URISyntaxException | IOException ex) {
 			LOG.error(ex);
 		} finally {
-            HttpClientUtils.closeQuietly(response);
+			HttpClientUtils.closeQuietly(response);
 		}
 		return json;
 	}
@@ -262,9 +313,9 @@ public class RestConnector {
 	 * @throws IOException
 	 */
 	@SuppressWarnings("resource")
-    public <T> T callGoDotComPost(Map<String, String> tokenList,
+	public <T> T callGoDotComPost(Map<String, String> tokenList,
 			String ctxPath, Class<T> objectType) throws IOException {
-        HttpResponse response = null;
+		HttpResponse response = null;
 		HttpPost postRequest = new HttpPost();
 		T tokenResp = null;
 		try {
@@ -288,14 +339,14 @@ public class RestConnector {
 					builder.setParameter(AuthConstants.USER_NAME,
 							tokenList.get(AuthConstants.USER_NAME));
 				}
-                if (tokenList.get(AuthConstants.SCOPE) != null) {
-                    builder.setParameter(AuthConstants.SCOPE,
-                            tokenList.get(AuthConstants.SCOPE));
-                }
-                if (tokenList.get(AuthConstants.CLIENT_SECRET) != null) {
-                    builder.setParameter(AuthConstants.CLIENT_SECRET,
-                            tokenList.get(AuthConstants.CLIENT_SECRET));
-                }
+				if (tokenList.get(AuthConstants.SCOPE) != null) {
+					builder.setParameter(AuthConstants.SCOPE,
+							tokenList.get(AuthConstants.SCOPE));
+				}
+				if (tokenList.get(AuthConstants.CLIENT_SECRET) != null) {
+					builder.setParameter(AuthConstants.CLIENT_SECRET,
+							tokenList.get(AuthConstants.CLIENT_SECRET));
+				}
 			}
 
 			postRequest = new HttpPost(builder.build());
@@ -305,17 +356,15 @@ public class RestConnector {
 
 			if (statusCode == HttpServletResponse.SC_OK) {
 				InputStream iStream = response.getEntity().getContent();
-				if (objectType != null)
-				{
-				    // If return type is String, return the json response, otherwise parse into tokens
-	                if (objectType.getName().equals("java.lang.String"))
-	                {
-	                    tokenResp = (T) EntityUtils.toString(response.getEntity());
-	                }
-	                else
-	                {
-	                    tokenResp = this.mapper.readValue(iStream, objectType);
-	                }
+				if (objectType != null) {
+					// If return type is String, return the json response,
+					// otherwise parse into tokens
+					if (objectType.getName().equals("java.lang.String")) {
+						tokenResp = (T) EntityUtils.toString(response
+								.getEntity());
+					} else {
+						tokenResp = this.mapper.readValue(iStream, objectType);
+					}
 				}
 				iStream.close();
 			}
